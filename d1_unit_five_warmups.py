7d6224dc6599b85cf9754e4dfca6014ae4341dce
Jonathan <jonathan@iMac9.local>
2018-10-11 08:20:58 -0400

    options with loops

---
+++ d1_unit_five_warmups.py
@@ -0,0 +1,14 @@
+for x in range(5):
+    # 01234
+    print(x, end="")
+
+print()
+
+for number in range(2, 8):
+    # 234567
+    print(number, end=",")
+
+print()
+
+for zebra in range(10, 20, 2):
+    print(zebra, end=" ")
