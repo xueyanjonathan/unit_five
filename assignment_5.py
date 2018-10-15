31cf09c7ae081ed495e23fa4e100f0e3be101091
Jonathan <jonathan@iMac9.local>
2018-10-15 11:17:51 -0400

    Try to use while function to run the functions, moduling situations which in the third roll the dice might equal to the point or equal to 7

--- assignment_5.py
+++ assignment_5.py
@@ -1,21 +1,31 @@
 import random
 
-games = input("How many times do you want to play?")
 
 def craps():
+    roll_a_dice = random.randint(1, 6)
+    wins = 0
+    games = input("How many games do you want to play?")
     for x in range(int(games)):
-        roll_a_dice = random.randint(1,6)
         dice_one = roll_a_dice
         dice_two = roll_a_dice
         sum_one = dice_one + dice_two
-        print("You rolled a", dice_one, " and a", dice_two, ". Your sum is", sum_one, ".")
-        if sum == 7 or sum == 11:
-            print("You win.")
-        elif sum == 2 or sum == 3 or sum == 12:
-            print("You lose.")
+        if sum_one == 7 or sum_one == 11:
+            wins = wins + 1
+        elif sum_one == 2 or sum_one == 3 or sum_one == 12:
+            pass
         else:
             dice_three = roll_a_dice
             dice_four = roll_a_dice
             sum = dice_three + dice_four
+            while sum != sum_one or sum != 7:
+                dice_three = roll_a_dice
+                dice_four = roll_a_dice
+                sum = dice_three + dice_four
+        return wins, games
+
 
 def main():
+    wins, games = craps()
+    percentage = wins/games
+    print("You ")
+    print("You won ", percentage, "percent of the time.")
