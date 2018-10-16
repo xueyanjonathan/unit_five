4245c51786d903a573fef4b349c9cea91545a5b8
Jonathan <jonathan@iMac9.local>
2018-10-16 11:41:23 -0400

    infinite loop

---
+++ d4_unit_five_warmups.py
@@ -0,0 +1,10 @@
+while True:
+    answer = input("Do you want to play a game?")
+    if answer == "y":
+        print("Good! Let's start.")
+        break
+    elif answer == "n":
+        print("Ok, fine.")
+        break
+    else:
+        print("Your answer is not valid")
